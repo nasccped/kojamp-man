625f672d4c09fa0d7da8413d6b5191ec2d5a5a74
NASCC <pdbt.contact@gmail.com>
2024-08-30 10:20:48 -0300

    [ADDED] Testing a C file project insertion

---
+++ setup/kojamp.c
@@ -0,0 +1,23 @@
+/* ------------------------------------------------------------------
+ *
+ * kojamp.c
+ *
+ *    Here's the program C file that will be turned into an
+ *    executable!
+ *
+ *    Although the project is developed mainly in the Java language,
+ *    a binary file is needed for everything to work the way it
+ *    should. So, here is the program!
+ *
+ * ----------------------------------------------------------------*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+  // FIX: still in tests!
+
+  printf("Hello world, from C Lang\n");
+
+  return 0;
+}
