fbbe31f16072aba9d97a94c5487e55b36560c298
NASCC <pedro.main.tec@gmail.com>
2024-10-04 12:06:16 -0300

    [RENAMED] kojamp.c -> __kojamp__.c

---
+++ setup/__kojamp__.c
@@ -0,0 +1,22 @@
+/* ------------------------------------------------------------------
+ *
+ * kojamp.c
+ *
+ *    Here's the program C file that will be turned into an
+ *    executable!
+ *
+ *    Although the project is developed mainly in the Java language,
+ *    a binary file is needed for everything to work the way it
+ *    should. So, here is the program!
+ *
+ * ----------------------------------------------------------------*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+
+  printf("Now, it's \033[35m__kojamp__.c\033[0m\n");
+
+  return 0;
+}
