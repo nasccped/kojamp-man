603a3f8bde72987dd050a1e44e481b48b1d92883
NASCC <pedro.main.tec@gmail.com>
2024-10-11 13:08:17 -0300

    [UPDATED] Commenting each include

--- setup/headers/_include_.h
+++ setup/headers/_include_.h
@@ -1,22 +1,27 @@
-#ifndef __INCLUDE__H_
-#define __INCLUDE__H_
+#ifndef __INCLUDE__H_ // if _include_.h isn't already defined
+#define __INCLUDE__H_ // define it
 
+// include stdio
 #ifndef _STDIO_H_
   #include <stdio.h>
 #endif
 
+// include stdlib
 #ifndef _STDLIB_H_
   #include <stdlib.h>
 #endif
 
+// include string
 #ifndef _STRING_H_
   #include <string.h>
 #endif
 
+// include unistd
 #ifndef _UNISTD_H_
   #include <unistd.h>
 #endif
 
+// include stdbool
 #ifndef _STDBOOL_H_
   #include <stdbool.h>
 #endif
