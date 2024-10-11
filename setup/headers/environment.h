44485419983a28eea55ac73aab36dd7df89ddb1e
NASCC <pedro.main.tec@gmail.com>
2024-10-11 20:54:45 -0300

    [UPDATED] Expected directories names was added

--- setup/headers/environment.h
+++ setup/headers/environment.h
@@ -16,6 +16,12 @@ struct LogList environment_run(void);
   static const char *DIRECTORY_SEPARATOR = "/";
 #endif
 
+static const char *GO_BACK_A_DIRECTORY = ".."   ;
+static const char *EXPECTED_OUT_NAME   = "out"  ;
+static const char *EXPECTED_GIT_NAME   = ".git" ;
+static const char *EXPECTED_SRC_NAME   = "src"  ;
+static const char *EXPECTED_STP_NAME   = "setup";
+
 // max length that a directory path can store
 #define MAX_DIRECTORY_LENGTH 250
 
@@ -23,5 +29,7 @@ struct LogList environment_run(void);
 extern char KOJAMP_BINARY_DIRECTORY[MAX_DIRECTORY_LENGTH];
 extern char KOJAMP_OUTPUT_DIRECTORY[MAX_DIRECTORY_LENGTH];
 extern char KOJAMP_GIT_DIRECTORY[MAX_DIRECTORY_LENGTH]   ;
+extern char KOJAMP_SOURCE_DIRECTORY[MAX_DIRECTORY_LENGTH];
+extern char KOJAMP_SETUP_NAME[MAX_DIRECTORY_LENGTH]      ;
 
 #endif
