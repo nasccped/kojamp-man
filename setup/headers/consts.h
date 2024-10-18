2d71fa67b987a2eb0733931269ea84bf4ecf9b36
NASCC <pedro.main.tec@gmail.com>
2024-10-18 12:59:10 -0300

    [ADDED] Const values from the project will be added here

---
+++ setup/headers/consts.h
@@ -0,0 +1,10 @@
+#ifndef _CONSTS_H_
+#define _CONSTS_H_
+
+static const char *ESCAPE_RESET      = "\033[0m"   ;
+static const char *ESCAPE_DEFT_RED   = "\033[0;31m";
+static const char *ESCAPE_ITAL       = "\033[3m"   ;
+static const char *ESCAPE_BOLD_GREEN = "\033[1;32m";
+
+
+#endif
