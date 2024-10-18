0881107e43b0526685fe224a7452f66ad83f2e83
NASCC <pedro.main.tec@gmail.com>
2024-10-18 13:21:40 -0300

    [UPDATED] Using the new implements

--- setup/_kojamp.c
+++ setup/_kojamp.c
@@ -6,10 +6,38 @@
   #include "headers/binary.h"
 #endif
 
+#ifndef _FUNCTIONS_H_
+  #include "headers/functions.h"
+#endif
+
+#ifndef _CONSTS_H_
+  #include "headers/consts.h"
+#endif
 
 int main(int argc, char *argv[]) {
 
-  binary();
+  printf("\n");
+
+  if (argc > 1) {
+
+    char *arg = argv[1];
+
+    if (find_char_on_array(arg, BINARY_CMD_EXPECTED    ,
+                                BINARY_CMD_EXPECTED_LEN))
+      binary();
+    else {
+      printf("Can't find command to given argument: %s%s%s\n",
+             ESCAPE_DEFT_RED, arg, ESCAPE_RESET              );
+
+      printf("\nTry %skojamp --binary%s %s(or -B)%s instead!\n"        ,
+             ESCAPE_BOLD_GREEN, ESCAPE_RESET, ESCAPE_ITAL, ESCAPE_RESET);
+    }
+  }
+  else
+    printf("No argument given! Try kojamp %s<something>%s instead...\n",
+           ESCAPE_ITAL, ESCAPE_RESET);
+
+  printf("\n");
 
   return 0;
 }
--- setup/headers/binary.h
+++ setup/headers/binary.h
@@ -3,4 +3,9 @@
 
 void binary(void);
 
+
+static const char *BINARY_CMD_EXPECTED[]   = {"--binary", "-B"};
+static const int   BINARY_CMD_EXPECTED_LEN = (
+  sizeof(BINARY_CMD_EXPECTED) / sizeof(BINARY_CMD_EXPECTED[0]));
+
 #endif
