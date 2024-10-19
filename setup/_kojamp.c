65edfe4beecf345ed37b50892ede3319f6554fd3
NASCC <pedro.main.tec@gmail.com>
2024-10-19 09:21:39 -0300

    [UPDATED] New print alerts when trying commands

--- setup/_kojamp.c
+++ setup/_kojamp.c
@@ -1,3 +1,4 @@
+// conditional includes to prevent redundant imports
 #ifndef __INCLUDE__H_
   #include "headers/_include_.h"
 #endif
@@ -14,30 +15,40 @@
   #include "headers/consts.h"
 #endif
 
+// main function
 int main(int argc, char *argv[]) {
 
   printf("\n");
 
+  // if at least one arg was provided
   if (argc > 1) {
 
+    // take that arg and continues
     char *arg = argv[1];
 
     if (find_char_on_array(arg, BINARY_CMD_EXPECTED    ,
                                 BINARY_CMD_EXPECTED_LEN))
       binary();
-    else {
-      printf("Can't find command to given argument: %s%s%s\n",
-             ESCAPE_DEFT_RED, arg, ESCAPE_RESET              );
 
+    else {
+      printf("Can't find command to given argument(s): %s",
+             ESCAPE_DEFT_RED);
+
+      for (int i = 1; i < argc; i++)
+        printf("%s ", argv[i]);
+
+      printf("%s\n", ESCAPE_RESET);
       printf("\nTry %skojamp --binary%s %s(or -B)%s instead!\n"        ,
              ESCAPE_BOLD_GREEN, ESCAPE_RESET, ESCAPE_ITAL, ESCAPE_RESET);
     }
-  }
-  else
+
+  } else
     printf("No argument given! Try kojamp %s<something>%s instead...\n",
            ESCAPE_ITAL, ESCAPE_RESET);
 
+  // break a line
   printf("\n");
 
+  // return int
   return 0;
 }
